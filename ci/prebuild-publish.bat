1b8cc4592348474cd6d7e1fde0c02771dc9fddd6
mderrier <maximilien.derrier@gmail.com>
2021-11-19 12:00:01 +0100

    feat(node): node16 build

--- ci/prebuild-publish.bat
+++ ci/prebuild-publish.bat
@@ -1,13 +1,12 @@
 @echo off
 
-if %1 == 14 (
-  if %APPVEYOR_REPO_BRANCH% == master (
-    if %GITHUB_TOKEN% neq "" (
-      npm run prebuild-node -- -u %GITHUB_TOKEN%
-      npm run prebuild-node-ia32 -- -u %GITHUB_TOKEN%
-      npm run prebuild-electron -- -u %GITHUB_TOKEN%
-      npm run prebuild-electron-ia32 -- -u %GITHUB_TOKEN%
-      npm run prebuild-electron -- -u %GITHUB_TOKEN%
-    )
+if %APPVEYOR_REPO_BRANCH% == master (
+  if %GITHUB_TOKEN% neq "" (
+    npm run prebuild-node -- -u %GITHUB_TOKEN%
+    npm run prebuild-node-ia32 -- -u %GITHUB_TOKEN%
+    npm run prebuild-electron -- -u %GITHUB_TOKEN%
+    npm run prebuild-electron-ia32 -- -u %GITHUB_TOKEN%
+    npm run prebuild-electron -- -u %GITHUB_TOKEN%
   )
 )
+
